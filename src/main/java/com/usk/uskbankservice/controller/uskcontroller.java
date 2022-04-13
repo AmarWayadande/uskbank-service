67124cbe6ec5e0ecff3a2c50a194e73c0f4c48d2
AmarWayadande <amarwayadande3142.com>
2022-04-13 15:08:59 +0530

    v1

--- src/main/java/com/usk/uskbankservice/controller/uskcontroller.java
+++ src/main/java/com/usk/uskbankservice/controller/uskcontroller.java
@@ -1,8 +1,14 @@
 package com.usk.uskbankservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class uskcontroller {
-
+    @GetMapping("/getData")
+	public String getData()
+	{
+		return "Git/GitHub";
+		
+	}
 }
